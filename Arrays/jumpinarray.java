364fbed0fdf41d714f662b15851052b2e4775cd6
Milindverma24 <anshverma24112005@gmail.com>
2026-07-23 10:50:14 +0530

    javajumping

--- Arrays/jumpinarray.java
+++ Arrays/jumpinarray.java
@@ -1,4 +1,34 @@
 package Arrays;
+import java.util.Scanner;
 
 public class jumpinarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt();
+
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+        int jumps = 0;
+
+        while (index < n - 1) {
+            if (arr[index] == 0) {
+                System.out.println("Cannot move further");
+                return;
+            }
+
+            index = index + arr[index];
+            jumps++;
+        }
+
+        System.out.println(jumps);
+
+        sc.close();
+    }
 }
